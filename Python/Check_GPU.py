4eba84e483625700e5414e0dcb7bd9217369441f
Valentin <valentin.vignal@gmail.com>
2019-05-08 19:50:26 +0800

    Test to see how to use GPU

--- Python/Check_GPU.py
+++ Python/Check_GPU.py
@@ -6,7 +6,8 @@ import sys
 def main():
     def get_available_gpus():
         local_device_protos = device_lib.list_local_devices()
-        return [x.name for x in local_device_protos if x.device_type == 'GPU']
+        # return [x.name for x in local_device_protos if x.device_type == 'GPU']
+        return local_device_protos
     name = get_available_gpus()
     print("GPUS:{}".format(name))
 
